05417be699a344f18a66324e88d9d45cfd82cbca
Leandro Rey <leandro.rey@avature.net>
2021-01-12 20:48:16 -0300

    OOP Excercise 3 resolution

--- Lesson2-OOPBasis/1-ClassAndObjects/Practice/Exercise3.php
+++ Lesson2-OOPBasis/1-ClassAndObjects/Practice/Exercise3.php
@@ -24,4 +24,30 @@ echo $matu->sayHello(). "\n \n";
  *  We want our greeter class to be able to say goodbye. What would you do ? Commit the changes for this
  *  questions in this class.
  *
+ * hello, my name is Mariano;
+ * hello, my name is Matu;
+ *
  */
+
+Class Greeter {
+    private $name;
+
+    public function __construct(string $name) {
+        $this->name = $name;
+    }
+
+    public function sayHello() {
+        return 'hello, my name is ' . $this->name;
+    }
+
+    public function sayGoodbye() {
+        return 'Good bye ' . $this->name;
+    }
+}
+
+$mariano = new Greeter('Mariano');
+$matu = new Greeter('Matu');
+echo $mariano->sayHello() . "<br>";
+echo $mariano->sayGoodbye() . "<br><br>";
+echo $matu->sayHello() . "<br>";
+echo $matu->sayGoodbye() . "<br>";
