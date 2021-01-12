3e9dcdcc0dce531a95921909967c863b92ee8784
Leandro Rey <leandro.rey@avature.net>
2021-01-12 20:47:58 -0300

    OOP Excercise 1 resolution

--- Lesson2-OOPBasis/1-ClassAndObjects/Practice/Exercise1.php
+++ Lesson2-OOPBasis/1-ClassAndObjects/Practice/Exercise1.php
@@ -10,13 +10,27 @@ class Mariano {
 $mariano = new Mariano();
 echo $mariano->sayHello() . "\n \n";
 
+$mariano = new Mariano();
+echo $mariano->sayHello() . "\n \n";
 /*
  *  What is the output in the command line for this code ?
  *
- *  Your answer:
+ *  Your answer: hello
  *
  *  What is the output if you run this code twice? Commit a change if you think that you can improve it.
  *
- *  Your answer:
+ *  Your answer: va a decir hello dos veces.
  *
  */
+
+class Mariano {
+
+    public function sayHello($saludo, $name) {
+        echo "$saludo $name <br>";
+    }
+}
+
+$mariano = new Mariano();
+$mariano->sayHello("Hola como estás", "Leandro");
+
+/* Después otros improvements que le haría serían similares a los que se plantean en el ejercicio 2, agregando el nombre de la persona. Otro solución podría ser ejecutar el echo en el método */
