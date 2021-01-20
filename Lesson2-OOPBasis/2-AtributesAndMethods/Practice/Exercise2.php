f9696e8c349013f39b8e08e2f06c949444c799ea
Leandro Rey <leandro.rey@avature.net>
2021-01-20 12:39:27 -0300

    Excercise 2 Atributes

--- Lesson2-OOPBasis/2-AtributesAndMethods/Practice/Exercise2.php
+++ Lesson2-OOPBasis/2-AtributesAndMethods/Practice/Exercise2.php
@@ -20,16 +20,23 @@ echo $mariano->lastName . "\n \n";
  *  Paste here the output of the changes you've made
  */
 
+$leandro = new PublicAttribute();
+$leandro->firstName = 'Leandro';
+$leandro->lastName = 'Rey';
+
+echo $leandro->firstName . "\n \n";
+echo $leandro->lastName . "\n \n";
+
+/* Output: Leandro Rey */
+
 // Add the code so the lines have a different output in the console.
 
-echo $mariano->firstName . "\n \n";
-echo $mariano->lastName . "\n \n";
-
+echo $leandro->firstName . ' ' . $leandro->lastName;
 
 /*
  * Do you think there's any way to prevent the fact that anyone can change the values of these attributes?
  * How?
  *
- * Your answer:
+ * Your answer: Si hacemos el atributo privado solo se podría acceder al valor desde la clase PublicAttribute y no desde afuera.
  *
  */
